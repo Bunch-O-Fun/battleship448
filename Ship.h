a978e355f6961fcfe09ad9959e30c6105aa88720
Liam Connolly <liam.connolly@ku.edu>
2020-10-03 11:41:45 -0500

    Fixed documentation

--- Ship.h
+++ Ship.h
@@ -27,60 +27,60 @@ class Ship {
         //Destructor
         ~Ship();
 
-        /*
-         *pre: none
-         *post: create a ship for desired length
-         *return: none
-        */
+        /**
+          * pre: none
+          * post: create a ship for desired length
+          * return: none
+          */
         void createShip(int length);
-        /*
-         *pre: none
-         *post: set coordinate for ship
-         *return: none
-        */
+        /**
+          *pre: none
+          *post: set coordinate for ship
+          *return: none
+          */
         void setCoordinate(string coordinate, int index);
-        /*
-         *pre: none
-         *post: get the coordinate for ship
-         *return: the coordinates for ship
-        */
+        /**
+          *pre: none
+          *post: get the coordinate for ship
+          *return: the coordinates for ship
+          */
         string getCoordinate(int index) const;
-        /*
-         *pre: none
-         *post: Set the ship length
-         *return: none
-        */
+        /**
+          *pre: none
+          *post: Set the ship length
+          *return: none
+          */
         void setLength(int length);
-        /*
-         *pre: none
-         *post: Get the ship length
-         *return: length of the ship
-        */
+        /**
+          *pre: none
+          *post: Get the ship length
+          *return: length of the ship
+          */
         int getLength() const;
 
-        /*
-         *pre: none
-         *post: boolean check the status of the ship
-         *return: true of the ship sunk, otherwise return false
-        */
+        /**
+          *pre: none
+          *post: boolean check the status of the ship
+          *return: true of the ship sunk, otherwise return false
+          */
         bool isSunk();
-        /*
-         *pre: none
-         *post: hit the enemy ship and give damage
-         *return: none
-        */
+        /**
+          *pre: none
+          *post: hit the enemy ship and give damage
+          *return: none
+          */
         void addDamage();
-        /*
-         *pre: none
-         *post: get the damage to the ship
-         *return: the damage to the ship
-        */
+        /**
+          *pre: none
+          *post: get the damage to the ship
+          *return: the damage to the ship
+          */
         int getDamage() const;
-        /*
-         *pre: none
-         *post: set the damage to the ship
-         *return: none
-        */
+        /**
+          *pre: none
+          *post: set the damage to the ship
+          *return: none
+          */
         void setDamage(int newDamage);
 
 };
